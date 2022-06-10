76ee87a3830d123dcade86d74d924072935a07d8
Devin Jansa <devinjansa@Devins-MacBook-Pro-2.local>
2022-06-10 10:30:31 -0700

    nearly finished, ready for first application to personal website

--- experiment.js
+++ experiment.js
@@ -5,18 +5,15 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 // access to sprite sheet
 
-
 const jelly = new Image();
 // Initialize random starting postion
 var x = Math.random() * innerWidth;
 var y = Math.random() * innerHeight;
-
-currentJelly = "Images/Jelly_vert_all.svg";
-
+currentJelly = "Images/Jelly_six_1.svg"
 jelly.src = currentJelly;
 jelly.onload = loadImages;
 //There are 7 sprites in 1 column
-let cols = 3;
+let cols = 6;
 let rows = 7;
 //To play the animation
 let totalFrames = 7;
@@ -29,6 +26,10 @@ var dx = Math.random() - 0.1;
 var dy = Math.random() - 0.1;
 //Record the number of times the "animate" function has been called
 let framesDrawn = 0;
+// //Find size of individiual sprites
+let spriteWidth = 0
+let spriteHeight = 0
+
 // Give our Jelly a random rotation
 // ctx.moveTo(x, y)
 // ctx.clearRect(0, 0, canvas.width, canvas.height); //content of previous frame cleared
@@ -38,17 +39,14 @@ let framesDrawn = 0;
 
 
 function animate() {
-  // jelly.src = currentJelly
-
   ctx.clearRect(0, 0, canvas.width * 2, canvas.height * 2); //content of previous frame cleared
-
   requestAnimationFrame(animate); //function will be called repeatedly on each new frame
   // //Find size of individiual sprites
   let spriteWidth = jelly.width / cols;
   let spriteHeight = jelly.height / rows;
 
   currentFrame = currentFrame % totalFrames; //Find current frame
-  srcY = currentFrame * spriteHeight; //Src position is updated to show newxt frame
+  srcY = currentFrame * spriteHeight; //Src position is updated to show next frame
 
   // image, srcX, srcY, srcWidth, srcHeight, destX, destY, destWdith, destHeight7
   ctx.drawImage(
@@ -62,10 +60,8 @@ function animate() {
     spriteWidth,
     spriteHeight
   );
-  //Ellipse enclosing the Jelly for hover effects
-  ctx.ellipse(x + 100, y + 100, 100, 60, null, Math.PI / 4, null, false)
 
-  // ctx.drawImage(jelly, srcX, srcY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
+  //Controlls the speed at which the frames roll
   framesDrawn++;
   if (framesDrawn >= 10) {
     currentFrame++;
@@ -74,7 +70,7 @@ function animate() {
   // Add the velocity as the speed out jellies move
   x += dx;
   y -= dy;
-  // Adjust where are jellies are inside the canvas element
+  // Keep the Jellies inside the window
   if (x > innerWidth || x < 0) {
     dx = -dx;
   }
@@ -84,17 +80,20 @@ function animate() {
   // Make the jelly change color depending on his location on the page
   if (y < canvas.height * 0.3) {
     srcX = 0 * spriteWidth;
-  } else if (y > canvas.height * 0.3 && y < canvas.height * 0.65) {
+  } 
+  else if (y > canvas.height * 0.3 && y < canvas.height * 0.65) {
     srcX = 1 * spriteWidth;
   } else {
     srcX = 2 * spriteWidth;
   }
+  if (hoverState === true) {
+    srcX = srcX + 900
+    // x += dx * 5
+    // y += dy * 5
+  }
+
 
 }
-
-//to apply hover effects to the Jelly
-
-
 //renders the image before the canvas
 let numOfImages = 1;
 function loadImages() {
@@ -102,3 +101,11 @@ function loadImages() {
   animate();
 }
 
+// Create a glow effect for when we hover over our Jelleis
+addEventListener('mousemove', (event) => {
+  if (x - event.clientX < 0 && event.clientX - (x + 200) < 0 && y - event.clientY < -60 && event.clientY - (y + 140) < 0) {
+    hoverState = true
+  } else if (!(x - event.clientX < 0 && event.clientX - (x + 200) < 0 && y - event.clientY < -60 && event.clientY - (y + 140) < 0)) {
+    hoverState = false
+  }
+});
